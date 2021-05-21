c891a343c094a1d92046766f2b3e2be047e2af4b
luckywzy <18792470504@163.com>
2021-05-21 23:53:58 +0800

    fix 161

--- src/main/java/com/leetcode/Solution161.java
+++ src/main/java/com/leetcode/Solution161.java
@@ -9,26 +9,27 @@ public class Solution161 {
     /**
      * https://leetcode-cn.com/problems/one-edit-distance/
      *
+     * 解法：分情况讨论即可
+     * case1：s.len < t.len, t 在 0..i的位置上删除一个字符，是否和s相等
+     * case2：s.len > t.len, 将case1反转
+     * case2: s.len == t.len, 将s中位置i的字符，替换为t中位置1的字符，判断是否相等
+     * 备注：java 中生成subStr需要遍历字符串，性能上有折扣
      * @param s
      * @return
      */
-    public boolean isOneEditDistance(String s, String t) {
-        return isOneDis(s, t, 0);
-    }
-
-    public String removeOneChar(String s, int index) {
-        return s.substring(0, index) + s.substring(index + 1);
-    }
-
-    public boolean isOneDis(String a, String b, int index) {
-        if (a.length() == b.length() && a.length() == 0 || Math.abs(a.length() - b.length()) > 1) {
+    public boolean isOneEditDistance1(String s, String t) {
+        if (s.length() == t.length() && s.length() == 0 ||
+                Math.abs(s.length() - t.length()) > 1 ||
+                s.equals(t)) {
             return false;
         }
-
-        index++;
-        return a.length() > b.length() ? isOneDis(removeOneChar(a, index), b, index) : (
-                a.length() < b.length() ? isOneDis(a, removeOneChar(b, index), index) :
-                        replaceOneIsSame(a, b));
+        if (s.length() < t.length()) {
+            return lessOne(s, t);
+        }
+        if (s.length() > t.length()) {
+            return moreOne(s, t);
+        }
+        return replaceOneIsSame(s, t);
     }
 
     private boolean replaceOneIsSame(String a, String b) {
@@ -44,4 +45,41 @@ public class Solution161 {
         return true;
     }
 
+    private boolean lessOne(String a, String b) {
+        return moreOne(b, a);
+    }
+
+    private boolean moreOne(String a, String b) {
+        for (int i = 0; i < a.length(); i++) {
+            if (b.equals(a.substring(0, i) + a.substring(i + 1))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public boolean isOneEditDistance2(String s, String t) {
+        if (s.length() == t.length() && s.length() == 0 ||
+                Math.abs(s.length() - t.length()) > 1 ||
+                s.equals(t)) {
+            return false;
+        }
+        if (s.length() > t.length()) {
+            return isOneEditDistance2(t, s);
+        }
+        //s < l
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) != t.charAt(i)) {
+                if (s.length() == t.length()) {
+                    return t.substring(i).equals(t.charAt(i) + s.substring(i + 1));
+                }
+                return s.substring(i).equals(t.substring(i + 1)) ||
+                        t.substring(i).equals(s.substring(i + 1));
+
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/com/leetcode/Solution161Test.java
+++ src/test/java/com/leetcode/Solution161Test.java
@@ -1,6 +1,5 @@
 package com.leetcode;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -14,12 +13,32 @@ public class Solution161Test {
 
     @Test
     public void isOneEditDistance() {
-        assertTrue(new Solution161().isOneEditDistance("a", "b"));
-        assertTrue(new Solution161().isOneEditDistance("ab", "b"));
-        assertTrue(new Solution161().isOneEditDistance("a", "ab"));
-        assertTrue(new Solution161().isOneEditDistance("ac", "bc"));
-        assertTrue(new Solution161().isOneEditDistance("acb", "abb"));
-        assertFalse(new Solution161().isOneEditDistance("acd", "bc"));
-        assertTrue(new Solution161().isOneEditDistance("aaa", "baa"));
+        assertTrue(new Solution161().isOneEditDistance1("a", "b"));
+        assertTrue(new Solution161().isOneEditDistance1("ab", "b"));
+        assertTrue(new Solution161().isOneEditDistance1("a", "ab"));
+        assertTrue(new Solution161().isOneEditDistance1("ac", "bc"));
+        assertTrue(new Solution161().isOneEditDistance1("acb", "abb"));
+        assertFalse(new Solution161().isOneEditDistance1("acd", "bc"));
+        assertTrue(new Solution161().isOneEditDistance1("aaa", "baa"));
+        assertTrue(new Solution161().isOneEditDistance1("a", ""));
+        assertTrue(new Solution161().isOneEditDistance1("", "a"));
+        assertFalse(new Solution161().isOneEditDistance1("c", "c"));
+        assertFalse(new Solution161().isOneEditDistance1("ab", "acd"));
+        assertTrue(new Solution161().isOneEditDistance1("ab", "acb"));
+        assertTrue(new Solution161().isOneEditDistance1("1203", "1213"));
+    }
+    @Test
+    public void isOneEditDistance2() {
+        assertTrue(new Solution161().isOneEditDistance2("a", "b"));
+        assertTrue(new Solution161().isOneEditDistance2("ab", "b"));
+        assertTrue(new Solution161().isOneEditDistance2("a", "ab"));
+        assertTrue(new Solution161().isOneEditDistance2("ac", "bc"));
+        assertTrue(new Solution161().isOneEditDistance2("acb", "abb"));
+        assertFalse(new Solution161().isOneEditDistance2("acd", "bc"));
+        assertTrue(new Solution161().isOneEditDistance2("aaa", "baa"));
+        assertFalse(new Solution161().isOneEditDistance2("c", "c"));
+        assertFalse(new Solution161().isOneEditDistance2("ab", "acd"));
+        assertTrue(new Solution161().isOneEditDistance2("ab", "acb"));
+        assertTrue(new Solution161().isOneEditDistance2("1203", "1213"));
     }
 }
